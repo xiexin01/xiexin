3b9670183b2eda005f013f7833bf9159b5947396
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-22 11:00:27 +0800

    Commit all changes including additions

---
+++ teauc/SO Archive/com/ztesoft/zsmart/ap/teauc/oss_iom/soarchive/TeaucPlugin1.java
@@ -0,0 +1,7 @@
+package com.ztesoft.zsmart.ap.teauc.oss_iom.soarchive;
+
+import com.ztesoft.zsmart.oss.core.om.itf.IOrderArchiveComponent;
+
+public class TeaucPlugin1 implements IOrderArchiveComponent {
+
+}
