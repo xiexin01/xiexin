0aaa398dc7a43d4fa1fd667d350058d84b5a7d09
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-28 11:29:49 +0800

    Commit all changes including additions

---
+++ zztest01/Product Split/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/prosplit/ProductSplitPlugin.java
@@ -0,0 +1,7 @@
+package com.ztesoft.zsmart.oss.np.zztest01.oss_iom.prosplit;
+
+import com.ztesoft.zsmart.oss.core.om.itf.IConditionComponent;
+
+public class ProductSplitPlugin implements IConditionComponent {
+
+}
