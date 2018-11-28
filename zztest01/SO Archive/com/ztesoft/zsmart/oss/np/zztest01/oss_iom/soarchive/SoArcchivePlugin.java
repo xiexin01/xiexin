95ea3cd1163f02ad12d20db9a51cd6ec5477fd0b
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-28 12:52:30 +0800

    Commit all changes including additions

---
+++ zztest01/SO Archive/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/soarchive/SoArcchivePlugin.java
@@ -0,0 +1,7 @@
+package com.ztesoft.zsmart.oss.np.zztest01.oss_iom.soarchive;
+
+import com.ztesoft.zsmart.oss.core.om.itf.IOrderArchiveComponent;
+
+public class SoArcchivePlugin implements IOrderArchiveComponent {
+
+}
