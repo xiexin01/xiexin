fb18ff68b5886163dfb669f05cef2688383ac379
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-29 10:50:31 +0800

    Commit changes to all files

--- zztest01/Work Order Split/com/ztesoft/zsmart/oss/np/zztest01/oss_isap/wosplit/WorkOrderSplitPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.ztesoft.zsmart.oss.np.zztest01.oss_isap.wosplit;
-
-import com.ztesoft.zsmart.oss.core.om.itf.IConditionComponent;
-
-public class WorkOrderSplitPlugin implements IConditionComponent {
-
-}
