a8979381eb3bd14aaf955ef71657b84e41d10377
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-29 18:18:47 +0800

    Commit all changes including additions

--- zztest01/Work Order Execute/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/woexec/WorkOrderExePlugin.java
+++ zztest01/Work Order Execute/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/woexec/WorkOrderExePlugin.java
@@ -3,5 +3,5 @@ package com.ztesoft.zsmart.oss.np.zztest01.oss_iom.woexec;
 import com.ztesoft.zsmart.oss.core.om.itf.ITaskOperationComponent;
 
 public class WorkOrderExePlugin implements ITaskOperationComponent {
-
+//Test
 }
