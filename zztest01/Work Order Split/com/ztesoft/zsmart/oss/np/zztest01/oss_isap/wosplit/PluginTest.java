5881d01b249be963b9ff5522bb61319e0188266f
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-11-29 09:46:03 +0800

    Commit all changes including additions

--- zztest01/Work Order Split/com/ztesoft/zsmart/oss/np/zztest01/oss_isap/wosplit/PluginTest.java
+++ zztest01/Work Order Split/com/ztesoft/zsmart/oss/np/zztest01/oss_isap/wosplit/PluginTest.java
@@ -3,5 +3,5 @@ package com.ztesoft.zsmart.oss.np.zztest01.oss_isap.wosplit;
 import com.ztesoft.zsmart.oss.core.om.itf.IConditionComponent;
 
 public class PluginTest implements IConditionComponent {
-//testTest
+//testTestTest
 }
