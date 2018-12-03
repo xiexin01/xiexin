2a353b5008c3aa19f33ad7e9c14203511c869999
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-12-03 12:12:43 +0800

    Commit all changes including additions

---
+++ zztest01/Work Order Create/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/wocreate/WorkOrderCreatePlugin.java
@@ -0,0 +1,13 @@
+package com.ztesoft.zsmart.oss.np.zztest01.oss_iom.wocreate;
+
+import com.ztesoft.zsmart.oss.core.om.workorder.domain.AbstractGenerationRule;
+
+public class WorkOrderCreatePlugin extends AbstractGenerationRule {
+
+	@Override
+	public void Test() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
