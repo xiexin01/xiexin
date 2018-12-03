fa8ff4d41ab2dfa7c459415a40d5737b9c515e47
xiexin01 <41788131+xiexin01@users.noreply.github.com>
2018-12-03 12:01:32 +0800

    Update WorkOrderExePlugin.java

--- zztest01/Work Order Execute/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/woexec/WorkOrderExePlugin.java
+++ zztest01/Work Order Execute/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/woexec/WorkOrderExePlugin.java
@@ -4,10 +4,4 @@ import com.ztesoft.zsmart.oss.core.om.itf.ITaskOperationComponent;
 
 public class WorkOrderExePlugin implements ITaskOperationComponent {
 
-	@Override
-	public void Test() {
-		// TODO Auto-generated method stub
-		
-	}
-//Test
 }
