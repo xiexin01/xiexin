f93034aa6b26ad79ef474951e2ac65794d7b6c80
liuxiongbiao <liu.xiongbiao@ztesoft.com>
2018-12-03 12:09:08 +0800

    Commit changes to all files

--- zztest01/Work Order Create/com/ztesoft/zsmart/oss/np/zztest01/oss_iom/wocreate/WorkOrderCrePlugin.java
+++
@@ -1,7 +0,0 @@
-package com.ztesoft.zsmart.oss.np.zztest01.oss_iom.wocreate;
-
-import com.ztesoft.zsmart.oss.core.om.workorder.domain.AbstractGenerationRule;
-
-public class WorkOrderCrePlugin extends AbstractGenerationRule {
-//Test
-}
