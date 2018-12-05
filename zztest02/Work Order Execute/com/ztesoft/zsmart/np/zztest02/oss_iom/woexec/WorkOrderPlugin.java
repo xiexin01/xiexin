6653c87e0138d8f9423cec8d8fbcd78d2cc7e09f
xiexin01 <41788131+xiexin01@users.noreply.github.com>
2018-12-05 16:03:31 +0800

    Delete WorkOrderPlugin.java

--- zztest02/Work Order Execute/com/ztesoft/zsmart/np/zztest02/oss_iom/woexec/WorkOrderPlugin.java
+++
@@ -1,7 +0,0 @@
-package com.ztesoft.zsmart.np.zztest02.oss_iom.woexec;
-
-import com.ztesoft.zsmart.oss.core.om.itf.ITaskOperationComponent;
-
-public class WorkOrderPlugin implements ITaskOperationComponent {
-//Test
-}
