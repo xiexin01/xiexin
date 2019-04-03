cb38a0b20954b8081b59265f85d21fd4cc137b41
xiexin <xie.xin4@ztesoft.com>
2019-04-03 10:35:58 +0800

    11

--- bsdk-sample/src/main/java/com/ztesoft/zsmart/oss/opb/bsdk/controller/PluginDemoController.java
+++ bsdk-sample/src/main/java/com/ztesoft/zsmart/oss/opb/bsdk/controller/PluginDemoController.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ztesoft.zsmart.core.exception.BaseAppException;
 import com.ztesoft.zsmart.oss.opb.bsdk.dto.PluginDemoDto;
@@ -18,7 +18,7 @@ import com.ztesoft.zsmart.oss.opb.plugin.PluginFactory;
 public class PluginDemoController {
 
     @RequestMapping(value = "queryPluginList", method = RequestMethod.GET)
-    public List<PluginDemoDto> queryPluginDemoList(String argCode, String argValue, String compType) throws BaseAppException {
+    public List<PluginDemoDto> queryPluginDemoList(@RequestParam(name="argCode", required=true) String argCode, @RequestParam(name="argValue", required=true) String argValue, @RequestParam(name="compType", required=true) String compType) throws BaseAppException {
         PluginFactory pf = PluginFactory.getPluginFactory();
         Map<String, Object> map = new HashMap<>();
         map.put(argCode, argValue);
